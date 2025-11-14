b2eafcd6887265c0f6e3ce9fe695719534caf875
EdWander <edwandersilva@gmail.com>
2025-11-14 03:20:48 -0300

    feat(application): casos de uso de pedido (criar, listar, obter, remover)

---
+++ src/GestaoPedidos.Application/Pedidos/CriarPedidoRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace GestaoPedidos.Application.Pedidos.UseCases;
+
+public class CriarPedidoItemRequest
+{
+    public Guid ProdutoId { get; set; }
+    public int Quantidade { get; set; }
+}
+
+public class CriarPedidoRequest
+{
+    public Guid ClienteId { get; set; }
+    public List<CriarPedidoItemRequest> Itens { get; set; } = new();
+}
