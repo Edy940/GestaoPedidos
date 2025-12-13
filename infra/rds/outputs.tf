0570fe6963c3a02f752751ec19c6d2939ed73bef
EdWander <edwandersilva@gmail.com>
2025-12-12 23:54:36 -0300

    Adiciona README e outras melhorias

--- infra/rds/outputs.tf
+++ infra/rds/outputs.tf
@@ -9,3 +9,9 @@ output "db_port" {
 output "db_name" {
   value = aws_db_instance.postgres.db_name
 }
+
+output "product_images_bucket_name" {
+  description = "Nome do bucket S3 onde as fotos de produtos são armazenadas"
+  value       = aws_s3_bucket.product_images.bucket
+}
+
--- infra/rds/terraform.tfvars
+++ infra/rds/terraform.tfvars
@@ -1,1 +1,2 @@
-db_password = "DbGestao123#"
+db_password = "DbGestao1234#"
+product_images_bucket_name = "gestaopedidos-produtos-edy940-dev"
